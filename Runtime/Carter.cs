efacc633598a9582b90a2f32fc5819f453a2b434
Huw Prosser <huw@carterapi.com>
2023-04-01 01:01:47 +0100

    Adds working package

--- Runtime/Carter.cs
+++ Runtime/Carter.cs
@@ -1,207 +1,75 @@
-using System;
+using System.Collections;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-using SocketIOClient;
-using SocketIOClient.Newtonsoft.Json;
-
-
 using UnityEngine.Networking;
-using System.Collections;
-
 
 namespace Carter {
-
     [System.Serializable]
-    class AgentOutput {
-        public string text;
-        public string audio;
-    }
-
-    [System.Serializable]
-    class AgentResponse {
-        public AgentOutput output;
+    public class ApiResponse {
+        public ForcedBehaviour[] forced_behaviours;
         public string input;
+        public Output output;
     }
 
+    [System.Serializable]
+    public class ForcedBehaviour {
+        public string name;
+    }
 
+    [System.Serializable]
+    public class Output {
+        public bool audio;
+        public string text;
+    }
+
+    [System.Serializable]
     public class Agent : MonoBehaviour
     {
-        public SocketIOUnity socket;
+        public string url;
+        public string key;
+        public string playerId;
 
-        public string url { get; set; }
-        public string apiKey { get; set; }
-        public string playerId { get; set; }
-        public string agentId { get; set; }
-        public string hash {get;set;}
-
-        public Boolean connected { get; set; }
-        public Boolean voice = true;
-
-        public delegate void OnMessage(string message);
-        public delegate void OnConnect();
-        public delegate void OnDisconnect();
-        public delegate void OnVoice(string audioId);
-
-        OnConnect onConnect { get; set;}
-        OnDisconnect onDisconnect { get; set;}
-        OnMessage onMessage { get; set;}
-        OnMessage onDictate { get; set;}
-        OnVoice onVoice { get; set;}
-        Listener listener;
+        public delegate void MessageEventHandler(ApiResponse response);
+        public event MessageEventHandler onMessage;
 
 
-
-
-        public void StartListening(){
-            listener = gameObject.AddComponent<Listener>();
+        [System.Serializable]
+        public class Message {
+            public string key;
+            public string playerId;
+            public string text;
         }
 
-
-        public void init(string apiKey, string playerId, string url, OnConnect onConnect, OnDisconnect onDisconnect, OnMessage onMessage, OnVoice onVoice, OnMessage onDictate = null)
-        {
-            this.apiKey = apiKey;
-            this.agentId = agentId;
-            this.url = url;
-            this.playerId = playerId;
-            this.onConnect = onConnect;
-            this.onDisconnect = onDisconnect;
-            this.onMessage = onMessage;
-            this.onVoice = onVoice;
-            this.onDictate = onDictate;
-            
-            connect();
+        private void HandleApiResponse(ApiResponse apiResponse) {
+            onMessage?.Invoke(apiResponse);
         }
 
+        public void Interact(string message) {
+            Debug.Log("Sending message: " + message);
+            StartCoroutine(SendJsonRequest(message));
+        }
 
-        public void connect()
-        {
-
-            var uri = new Uri(url);
-            socket = new SocketIOUnity(uri, new SocketIOOptions
-            {
-                ExtraHeaders = new Dictionary<string, string>
-                    {
-                        ["key"] = this.apiKey,
-                        ["player"] = this.playerId
-                    }
-                ,
-                Transport = SocketIOClient.Transport.TransportProtocol.WebSocket
-            });
-
-            socket.JsonSerializer = new NewtonsoftJsonSerializer();
-
-            socket.OnConnected += (sender, e) =>
-            {
-                connected = true;
-                onConnect();
+        IEnumerator SendJsonRequest(string message) {
+            Message msg = new Message {
+                key = this.key,
+                playerId = this.playerId,
+                text = message
             };
+            string json = JsonUtility.ToJson(msg);
 
-            socket.OnDisconnected += (sender, e) =>
-            {
-                connected = false;
-                onDisconnect();
-            };
+            using (UnityWebRequest www = new UnityWebRequest(url, UnityWebRequest.kHttpVerbPOST)) {
+                www.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(json));
+                www.downloadHandler = new DownloadHandlerBuffer();
+                www.SetRequestHeader("Content-Type", "application/json");
 
-            socket.OnUnityThread("message", (data) =>
-            {
-                AgentResponse response = data.GetValue<AgentResponse>();
-
-                if(voice == true){
-                    onVoice(response.output.audio);
-                }
-
-                onMessage(response.output.text);
-            });
-
-            socket.OnUnityThread("hash", (data) =>
-            {
-                Debug.Log(data.GetValue<string>());
-                hash = data.GetValue<string>();
-            });
-
-
-            socket.OnUnityThread("dictate", (data) =>
-            {
-                Debug.Log(data.GetValue<string>());
-                onDictate(data.GetValue<string>());
-            });
-
-
-            socket.Connect();
-
-        }
-
-        public void disconnect() {
-            socket.Disconnect();
-        }
-
-        public void send(string message) {
-            if (socket == null) {
-                Debug.Log("Error sending message, socket is null");
-                return;
-            } else {
-                Debug.Log("{\"text\": \"" + message + "\", \"hash\": \"" + hash + "\" }");
-                socket.EmitStringAsJSON("message", "{\"text\": \"" + message + "\", \"hash\": \"" + hash + "\" }");
-            }
-        }
-
-
-        // AUDIO //
-
-        public void listen(){
-            listener.StartListening();
-        }
-
-        public string stopListening(){
-            return listener.StopListening();
-        }
-
-        public void say(string audioId){
-            StartCoroutine(PlayAudio(url + "/speak/" + audioId));
-        }
-
-        public void sendAudio(){
-
-            string base64 = stopListening();
-
-            if(base64 != null){
-                Debug.Log("Sending audio");
-
-                if (socket == null) {
-                    Debug.Log("Error sending message, socket is null");
-                    return;
-                } else {
-                    socket.EmitStringAsJSON("voice_message", "{\"audio\": \"" + base64 + "\", \"apiKey\": \"" + this.apiKey + "\", \"playerId\": \"" + this.playerId + "\"}");
-                }
-            }
-        }
-
-        public IEnumerator PlayAudio(string url)
-        {
-            Debug.Log("Playing audio from: " + url);
-        
-            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG))
-            {
                 yield return www.SendWebRequest();
 
-                if (www.result == UnityWebRequest.Result.ConnectionError)
-                {
+                if (www.result != UnityWebRequest.Result.Success) {
                     Debug.Log(www.error);
-                }
-                else
-                {
-                    // Debug.Log("Playing audio");
-                    AudioClip myClip = DownloadHandlerAudioClip.GetContent(www);
-                    AudioSource audioSource2 = gameObject.AddComponent<AudioSource>();
-                    audioSource2.clip = myClip;
-                    audioSource2.Play();
-                    www.Dispose();
+                } else {
+                    ApiResponse apiResponse = JsonUtility.FromJson<ApiResponse>(www.downloadHandler.text);
+                    HandleApiResponse(apiResponse);
                 }
             }
         }
-
-
     }
 }
